58aad9ea0b0da8b35fff8d8f1743d768685e6beb
Graham Keenan <graham.keenan@outlook.com>
2026-03-19 21:56:05 +0000

    parse paren

--- src/parse.rs
+++ src/parse.rs
@@ -16,7 +16,56 @@ impl<'de> Parser<'de> {
     }
 
     pub fn parse(&mut self) -> Result<Expr<'de>, Error> {
-        self.parse_bp(0)
+        self.parse_statement(0)
+    }
+
+    pub fn parse_statement(&mut self, min_bp: u8) -> Result<Expr<'de>, Error> {
+        let mut lhs = match self.lexer.next() {
+            Some(Ok(token)) => match token.class() {
+                Some(TokenClass::Atom(atom)) => Expr::Atom(atom),
+                Some(TokenClass::Op(op)) => {
+                    if op == Op::Group {
+                        let rest = self.parse_statement(0)?;
+                        eprintln!("REST: {rest}");
+                        Expr::Cons(op, vec![rest])
+                    } else {
+                        let ((), rbp) = prefix_binding_power(op);
+                        let rhs = self.parse_statement(rbp)?;
+                        Expr::Cons(op, vec![rhs])
+                    }
+                }
+                None => anyhow::bail!("expected atom - found None"),
+            },
+            None => return Ok(Expr::Atom(Atom::Nil)),
+            Some(Err(e)) => return Err(e.into()),
+        };
+
+        loop {
+            let op = self.lexer.peek();
+            if op.map_or(false, |op| op.is_err()) {
+                let err = self.lexer.next().expect("checked").expect_err("checked");
+                anyhow::bail!("encountered op parse error: {err}");
+            }
+
+            let op = match op.map(|res| res.as_ref().expect("checked err above")) {
+                Some(token) => match token.class() {
+                    Some(TokenClass::Op(op)) => op,
+                    None => break,
+                    _ => panic!("OP: {op:?}"),
+                },
+                None => break,
+            };
+
+            let (lbp, rbp) = infix_binding_power(op);
+            if lbp < min_bp {
+                break;
+            }
+            self.lexer.next();
+            let rhs = self.parse_bp(rbp)?;
+            lhs = Expr::Cons(op, vec![lhs, rhs])
+        }
+
+        Ok(lhs)
     }
 
     fn parse_bp(&mut self, min_bp: u8) -> Result<Expr<'de>, Error> {
@@ -64,8 +113,9 @@ impl<'de> Parser<'de> {
 
 fn prefix_binding_power(op: Op) -> ((), u8) {
     match op {
+        Op::Print | Op::Return => ((), 1),
         Op::Minus | Op::Bang => ((), 5),
-        _ => todo!(),
+        _ => panic!("bad op: {op:?}"),
     }
 }
 
@@ -80,7 +130,7 @@ fn infix_binding_power(op: Op) -> (u8, u8) {
         | Op::BangEqual
         | Op::EqualEqual => (5, 6),
         Op::Field => (8, 7),
-        _ => todo!(),
+        _ => panic!("infix bad op: {op:?}"),
     }
 }
 
