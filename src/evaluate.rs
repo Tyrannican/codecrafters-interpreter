b666f9c898fdb819a4a1efdc0e048775bf3bf6a5
Graham Keenan <graham.keenan@outlook.com>
2026-03-21 16:42:50 +0000

    evaluate simple stuff

--- src/evaluate.rs
+++ src/evaluate.rs
@@ -1,4 +1,7 @@
-use crate::parse::Ast;
+use crate::{
+    lex::{Atom, Op},
+    parse::Ast,
+};
 
 use anyhow::Result;
 
@@ -12,7 +15,29 @@ impl<'de> Evaluator<'de> {
     }
 
     pub fn evaluate(&mut self) -> Result<()> {
-        println!("EVALUATE");
+        match self.ast {
+            Ast::Cons(op, ref args) => self.parse_operation(op, args),
+            Ast::Atom(atom) => self.parse_atom(atom),
+        }
+
         Ok(())
+    }
+
+    fn parse_atom(&self, atom: Atom) {
+        match atom {
+            Atom::Ident(_) => {}
+            Atom::Number(n) => {
+                if n == n.trunc() {
+                    println!("{}", n as isize);
+                } else {
+                    println!("{n}");
+                }
+            }
+            atom => println!("{atom}"),
+        }
+    }
+
+    fn parse_operation(&self, op: Op, args: &[Ast]) {
+        todo!()
     }
 }
